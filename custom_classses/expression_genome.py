33d72c5755ef6326919d45d3bda88cb24a957027
Vasilis J Promponas <49338525+vprobon@users.noreply.github.com>
2026-03-09 16:59:09 +0200

    Create expression_genome.py
    A buggy python class for debugging

---
+++ custom_classses/expression_genome.py
@@ -0,0 +1,25 @@
+class ExpressionGenome(Genome):
+    '''This module needs debugging.'''
+
+    data_type = "RNA-seq"
+
+    def __init__(self, species)
+        super().__init__(species)
+        self.expression = []
+
+    def add_expression(self, gene, value):
+        self.expression[gene] = value
+
+    def mean_expression(self)
+
+        total = sum(self.expression.values())
+        return total / len(self.expression)
+
+    def highly_expressed(self, threshold):
+        genes = []
+
+        for gene, value in self.expression.items():
+            if value > threshold:
+                genes.append(value)
+
+        return genes
